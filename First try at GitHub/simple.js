0f3f4885211e5f596aa30545a0d6acf975b2fc37
anjms <anja_stensen@hotmail.com>
2024-02-21 10:56:58 +0100

    changing stuff for fun :(

--- First try at GitHub/simple.js
+++ First try at GitHub/simple.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-console.log("Do you come here often? ;)");
-=======
-console.log("Do you come here often? ;)");
->>>>>>> faecbb58f62406f1e7a2fe2ec8f6ff0c8a68bf14
+console.log("How am i suppposed to rememper i all :(");
